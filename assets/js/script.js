2137eaf92f50f58b5679ac80b77617ca58115203
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-27 20:18:04 +0100

    targeting the HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// target the HTML elements
+
+const keyPressedSpan = document.querySelectore("#key");
+const keyCodeSpan = document.querySelectorr("#code");
+const eventStatusSpan = document.querySelector("#status");
