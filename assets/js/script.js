d63b511d17aa07f05fdb346d3a8f08f2ab255eeb
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-27 20:32:38 +0100

    callback function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,17 @@ const keyPressedSpan = document.querySelectore("#key");
 const keyCodeSpan = document.querySelectorr("#code");
 const eventStatusSpan = document.querySelector("#status");
 
+//callback function
+const handleKeyDown = function (event) {
+  // get key pressed data from the event object
+  const keyPressed = event.key;
+  const keyCode = event.code;
+
+  // set text content of the spans
+  keyPressedSpan.textContent = keyPressed;
+  keyCodeSpan.TextContent = keyCode;
+  eventStatusSpan.textContent = "KEYDOWN Event";
+};
+
 // add key down event listener
 document.addEventListener("keydown", handleKeyDown);
