2e8d1bd9f558099082cc5f6ac9867a451b6dbbcf
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-27 20:20:35 +0100

    add key down event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,6 @@
 const keyPressedSpan = document.querySelectore("#key");
 const keyCodeSpan = document.querySelectorr("#code");
 const eventStatusSpan = document.querySelector("#status");
+
+// add key down event listener
+document.addEventListener("keydown", handleKeyDown);
